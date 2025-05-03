3b4ef6b5d881569f05d7c8d99bfc485851bf7c97
Christopher Wolf <>
2025-05-03 16:46:41 +0200

    small import fix

--- example/lib/onedrive_explorer.dart
+++ example/lib/onedrive_explorer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_onedrive/flutter_onedrive.dart';
+import 'package:flutter_onedrive/onedrive_file.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
